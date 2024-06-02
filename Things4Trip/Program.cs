9c087bc97a3e13f7abc407191807a4487e8425fa
LenkaSvob <LenkaLiberty@gmail.com>
2024-06-02 10:52:50 +0200

    Created new project

---
+++ Things4Trip/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Things4Trip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Things4Trip01");
+        }
+    }
+}
